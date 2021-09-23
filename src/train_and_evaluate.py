505f5ca447d1230bd21ca141769b9db04663589b
DeepranjanG <kdeepu070@gmail.com>
2021-09-23 10:50:49 +0530

    Stage2 complete

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -52,6 +52,18 @@ def train_and_evaluate(config_path):
     predicted_qualities = lr.predict(test_x)
     (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
+    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+
+    
+
+    os.makedirs(model_dir, exist_ok=True)
+    model_path = os.path.join(model_dir, "model.joblib")
+
+    joblib.dump(lr, model_path)
+
 
 
 
